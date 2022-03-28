1361cac5c422c5bcc6202cefde03e11450fcbded
abhi1465 <101691593+abhi1465@users.noreply.github.com>
2022-03-28 10:27:42 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,11 @@
+list1=[12, -7, 5, 64, -14]
+list2=[12,14,-95,3]
+print('for list1 ')
+for i in list1:
+    if i>=0:
+        print(i)
+
+print('for list2')
+for x in list2:
+    if x>=0:
+        print(x)
